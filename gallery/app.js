9b4770c2662a3e4a2d4e8e2b8db81bdb5812bcb3
Gustavo Pereira <xgustavux@hotmail.com>
2021-11-30 11:21:19 +1000

    gallerry done
    gallery done

--- gallery/app.js
+++ gallery/app.js
@@ -33,6 +33,11 @@ function Gallery(element){
             this.openModal(e.target, this.imageList);
         }
     }.bind(this));
+
+    this.closeModal = this.closeModal.bind(this);
+    this.prevImg = this.prevImg.bind(this);
+    this.nextImg = this.nextImg.bind(this);
+    this.chooseImage = this.chooseImage.bind(this);
 }
 
 Gallery.prototype.openModal = function(selectedImg, imageList){
@@ -47,12 +52,47 @@ Gallery.prototype.openModal = function(selectedImg, imageList){
                 />`;
     }).join("");
     this.modal.classList.add("open");
+    this.closeBtn.addEventListener("click", this.closeModal);
+    this.prevBtn.addEventListener("click", this.prevImg);
+    this.nextBtn.addEventListener("click", this.nextImg);
+    this.modalImages.addEventListener("click", this.chooseImage);
 };
 
 Gallery.prototype.setMainImage = function(selectedImg){
     this.modalMainImg.src = selectedImg.src;
     this.imgName.textContent = selectedImg.textContent;
     this.imgName.textContent = selectedImg.title;
+};
+
+Gallery.prototype.closeModal = function(){
+    this.modal.classList.remove("open");
+    this.closeBtn.removeEventListener("click", this.closeModal);
+    this.prevBtn.removeEventListener("click", this.prevImg);
+    this.nextBtn.removeEventListener("click", this.nextImg);
+};
+
+Gallery.prototype.chooseImage = function(e){
+    if(e.target.classList.contains("modal-img")){
+        this.setMainImage(e.target);
+    const selected = this.modalImages.querySelector(".selected");
+    selected.classList.remove("selected")
+    const currentImage = e.target;
+    currentImage.classList.add("selected");
+    }
+};
+Gallery.prototype.prevImg = function(){
+    const currentImage = this.modalImages.querySelector(".selected");
+    const nextImage = currentImage.previousElementSibling || this.modalImages.lastElementChild;
+    currentImage.classList.remove("selected");
+    nextImage.classList.add("selected");
+    this.setMainImage(nextImage);
+};
+Gallery.prototype.nextImg = function(){
+    const currentImage = this.modalImages.querySelector(".selected");
+    const nextImage = currentImage.nextElementSibling || this.modalImages.firstElementChild;
+    currentImage.classList.remove("selected");
+    nextImage.classList.add("selected");
+    this.setMainImage(nextImage);
 };
 
 const nature = new Gallery(getElement(".nature"));
