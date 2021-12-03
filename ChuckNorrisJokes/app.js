8e1f1b928041a1c081c2fbc59b733a089b7e488c
Gustavo Pereira <xgustavux@hotmail.com>
2021-12-03 22:45:15 +1000

    chuck norris dev jokes done
    simplest api integration possible.

--- ChuckNorrisJokes/app.js
+++ ChuckNorrisJokes/app.js
@@ -5,26 +5,32 @@ const content = document.querySelector(".content");
 const image = document.querySelector(".img");
 
 btn.addEventListener("click", () => {
-    getData(URL);
+    getData(URL)
+    .then(response => displayData(response))
+    .catch(err => console.log(err));
 });
 
 function getData(url){
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.send();
-    xhr.onreadystatechange = function(){
-        if(xhr.readyState !== 4) return;
-        if(xhr.status === 200){
-            image.classList.add("shake-img");
-            const {value:joke} = JSON.parse(xhr.responseText);
-            content.textContent = joke;
-            setTimeout(()=>{
-                image.classList.remove("shake-img");
-            }, 500);
-        } else {
-            console.log({status: xhr.status, text: xhr.statusText});
-        }
-    };
+    return new Promise((resolve, reject)=>{
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.send();
+        xhr.onreadystatechange = function(){
+            if(xhr.readyState !== 4) return;
+            if(xhr.status === 200){
+                resolve(xhr.responseText);
+            } else {
+                reject({status: xhr.status, text: xhr.statusText});
+            }
+        };
+    });
 }
 
-console.log(getData())
+function displayData(data){
+    image.classList.add("shake-img");
+    const { value: joke } = JSON.parse(data);
+    content.textContent = joke;
+    setTimeout(()=>{
+        image.classList.remove("shake-img");
+    }, 500);
+};
