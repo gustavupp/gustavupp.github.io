a54e7ba541cbb499c5c58d14179b5d63ff23388d
Gustavo Pereira <xgustavux@hotmail.com>
2021-12-04 23:26:56 +1000

    created and started working on a book search project
    created and started working on a book search project

--- ChuckNorrisJokes/app.js
+++ ChuckNorrisJokes/app.js
@@ -5,6 +5,36 @@ const content = document.querySelector(".content");
 const image = document.querySelector(".img");
 
 btn.addEventListener("click", () => {
+    fetch(URL).then(data);
+});
+
+function getData(url){
+    return new Promise((resolve, reject)=>{
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.send();
+        xhr.onreadystatechange = function(){
+            if(xhr.readyState !== 4) return;
+            if(xhr.status === 200){
+                resolve(xhr.responseText);
+            } else {
+                reject({status: xhr.status, text: xhr.statusText});
+            }
+        };
+    });
+}
+
+function displayData(data){
+    image.classList.add("shake-img");
+    const { value: joke } = JSON.parse(data);
+    content.textContent = joke;
+    setTimeout(()=>{
+        image.classList.remove("shake-img");
+    }, 500);
+};
+
+//with XMLHttpRequest
+/*btn.addEventListener("click", () => {
     getData(URL)
     .then(response => displayData(response))
     .catch(err => console.log(err));
@@ -34,3 +64,4 @@ function displayData(data){
         image.classList.remove("shake-img");
     }, 500);
 };
+*/
---
+++ book-lookUp/app.js
@@ -0,0 +1,68 @@
+//grab DOM elements
+const cardContainer = document.querySelector(".card-container");
+const bookImg = document.querySelector(".img"); 
+const bookTitle = document.querySelector(".title"); 
+const author = document.querySelector(".author"); 
+const bookDescription = document.querySelector(".description"); 
+const searchInput = document.querySelector("input");
+const btn = document.querySelector(".btn");
+const url = "https://www.googleapis.com/books/v1/volumes?q=";
+
+btn.addEventListener("click",(e)=> {
+    e.preventDefault();
+    const inputValue = searchInput.value.replace(" ", "+");
+    const searchUrl = `https://www.googleapis.com/books/v1/volumes?q=${inputValue}`;
+    getData(searchUrl)
+    .then(response => displayData(response))
+    .catch(err => console.log(err));
+});
+    
+function getData(url){
+    return new Promise((resolve, reject)=>{
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", url);
+        xhr.send();
+        xhr.onreadystatechange = function(){
+            if(xhr.readyState !== 4) return;
+            if (xhr.status === 200){
+                resolve(xhr.responseText);
+            } else {
+                reject({ 
+                    status: xhr.status,
+                    text: xhr.statusText, 
+                });
+            }
+        }
+    });
+}
+
+//display data function
+function displayData(data){
+    const searchData = JSON.parse(data);
+    const searchData1 = searchData.items
+
+    const result = searchData1.map((item)=>{
+        return `<div class="card">
+        <img src="${(item.volumeInfo.imageLinks)? item.volumeInfo.imageLinks.thumbnail : "https://dummyimage.com/120x150/000/fff.png&text=No+Image!"}" alt="image" class="img" style="max-width: 150px;">
+        <div class="book-info">
+            <h3 class="title">${item.volumeInfo.title}</h3>
+            <p class="author">${item.volumeInfo.authors}</p>
+            <p class="description">${item.searchInfo.textSnippet}</p>
+            <button class="read-more">Read More</button>
+        </div>
+        </div>`;
+    }).join("");
+    console.log(searchData1)
+    cardContainer.innerHTML = result;
+
+}
+
+    /*
+    function handleResponse(response) {
+      for (var i = 0; i < response.items.length; i++) {
+        var item = response.items[i];
+        content.innerHTML += `<br><img src="${(item.volumeInfo.imageLinks) ? img.src= item.volumeInfo.imageLinks.thumbnail : img.alt = "NO IMG :("}" alt="${item.volumeInfo.title}" class="img" style="max-width: 150px;">
+        <h3>${item.volumeInfo.title}</h3>` ;
+      }
+    }
+    */
