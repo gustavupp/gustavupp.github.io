36428bef7f56583b1ae1b3cfc36eaac61624ef44
Gustavo Pereira <xgustavux@hotmail.com>
2021-12-07 21:48:49 +1000

    size of thumbnails and icons adjusted for mobile view
    size of thumbnails and icons adjusted for mobile view

--- book-lookUp/utils/displayBookSearch.js
+++ book-lookUp/utils/displayBookSearch.js
@@ -4,7 +4,7 @@ function displayBookSearch(data){
         
     const result = searchData1.map((item)=>{
         return `<div class="card">
-                    <img src="${(item.volumeInfo.imageLinks)? item.volumeInfo.imageLinks.thumbnail : "https://dummyimage.com/120x150/000/fff.png&text=No+Cover!"}" alt="image" class="book-cover" data-id="${item.id}" style="max-width: 150px;" />
+                    <img src="${(item.volumeInfo.imageLinks)? item.volumeInfo.imageLinks.thumbnail : "https://dummyimage.com/120x150/000/fff.png&text=No+Cover!"}" alt="image" class="book-cover" data-id="${item.id}" />
                     <div class="book-info">
                     <p class="title">${item.volumeInfo.title.length > 12? item.volumeInfo.title.slice(0, 12) + "..." : item.volumeInfo.title.length}</p>
                 </div>
