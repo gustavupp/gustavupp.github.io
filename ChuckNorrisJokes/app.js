4aab0fd4c54264beaac835178455bd0d9cb1e094
Gustavo Pereira <xgustavux@hotmail.com>
2021-12-09 15:58:22 +1000

    chuck norris dev jokes fixed
    HMLHttp request fixed and added a fetch as well, just to practice

--- ChuckNorrisJokes/app.js
+++ ChuckNorrisJokes/app.js
@@ -5,8 +5,8 @@ const content = document.querySelector(".content");
 const image = document.querySelector(".img");
 
 btn.addEventListener("click", () => {
-    fetch(URL)
-    .then(displayData(data))
+    getData(URL)
+    .then(data => displayData(data))
     .catch(err => console.log(err));
 });
 
@@ -35,32 +35,17 @@ function displayData(data){
     }, 500);
 };
 
-//with XMLHttpRequest
-/*btn.addEventListener("click", () => {
-    getData(URL)
-    .then(response => displayData(response))
+//with fetch
+ /*btn.addEventListener("click", () => {
+    fetch(URL)
+    .then(response => response.json())
+    .then(parsedData => displayData(parsedData))
     .catch(err => console.log(err));
 });
 
-function getData(url){
-    return new Promise((resolve, reject)=>{
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
-        xhr.send();
-        xhr.onreadystatechange = function(){
-            if(xhr.readyState !== 4) return;
-            if(xhr.status === 200){
-                resolve(xhr.responseText);
-            } else {
-                reject({status: xhr.status, text: xhr.statusText});
-            }
-        };
-    });
-}
-
 function displayData(data){
     image.classList.add("shake-img");
-    const { value: joke } = JSON.parse(data);
+    const { value: joke } = data;
     content.textContent = joke;
     setTimeout(()=>{
         image.classList.remove("shake-img");
