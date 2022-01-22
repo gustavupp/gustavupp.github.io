23b35adc2c816866dd35c7724aeb939e1c879d76
Gustavo Pereira <xgustavux@hotmail.com>
2022-01-22 15:32:20 +1000

    duplicate books on bookShelf app fixed

--- book-lookUp/utils/addToLocalStorage.js
+++ book-lookUp/utils/addToLocalStorage.js
@@ -1,9 +1,31 @@
-import getLocalStorage from "./getLocalStorage.js";
+import getLocalStorage from './getLocalStorage.js'
 
-function addToLocalStorage(object){
-    let localStorageList = getLocalStorage(); //assign localstoragelist whatever is currently on local storage
-    localStorageList.push(object); //add new item to array
-    localStorage.setItem("bookList", JSON.stringify(localStorageList)); //set the whole thing back to local storage
+function addToLocalStorage(object) {
+  let localStorageList = getLocalStorage() //assign localstoragelist whatever is currently on local storage
+  localStorageList.push(object) //add new item to array
+
+  //filter the new array of objects so it doesn't contain duplicates
+  let newLocalStorageList = [
+    ...new Map(localStorageList.map((item) => [item.id, item])).values(),
+  ]
+
+  localStorage.setItem('bookList', JSON.stringify(newLocalStorageList)) //set the whole thing back to local storage
 }
 
-export default addToLocalStorage;
+export default addToLocalStorage
+
+/*********************ALTERNATIVE FOR GETTING AN ARRAY OF UNIQUE OBJECTS***********************/
+
+//a more readable implementation of the above code
+//let newLocalStorageList = localStorageList.map((item) => item[id], item)
+//let uniqueValuesLocalStorageList = [...new Map(newLocalStorageList).values()]
+
+/*----------------------------------------------------------------------------------------------*/
+
+// a more uld fashioned aproach would've filter the array as well
+//   let uniqueBooks = []
+//   for (let keys in localStorageList) {
+//     if (!uniqueBooks.includes(localStorageList[keys])) uniqueBooks.push(localStorageList[keys])
+//   }
+
+/************************************************************************************************/
