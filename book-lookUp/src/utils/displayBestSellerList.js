45077c67bbcbc20a4fa56be992db0c9099dcdd1e
Gustavo Pereira <xgustavux@hotmail.com>
2022-02-05 13:19:41 +1000

    new bookshelf project added to portifolio

--- book-lookUp/src/utils/displayBestSellerList.js
+++ book-lookUp/src/utils/displayBestSellerList.js
@@ -1,19 +1,27 @@
-import { nyCardContainer } from "../app.js"
+import { nyCardContainer } from '../app.js'
 
-function displayBestSellerList(isbnList){
-    let cardNode = document.createElement("div"); //create new "card" node for every call
-    cardNode.classList.add("card"); //add the "card" class
+function displayBestSellerList(isbnList) {
+  let cardNode = document.createElement('div') //create new "card" node for every call
+  cardNode.classList.add('card') //add the "card" class
 
-    //render this html content to the node
-    cardNode.innerHTML = `
-                            <img src="${(isbnList.thumbnail)? isbnList.thumbnail : "https://dummyimage.com/120x150/000/fff.png&text=No+Cover!"}" alt="image" class="book-cover" data-id="${isbnList.id}" />
-                            <div class="book-info">
-                                <p class="title">${isbnList.title.length > 11? isbnList.title.slice(0, 11) + "..." : isbnList.title}</p>
-                            </div>
-                        `;
+  //render this html content to the node
+  cardNode.innerHTML = `
+    <img src="${
+      isbnList.thumbnail
+        ? isbnList.thumbnail
+        : 'https://dummyimage.com/120x150/000/fff.png&text=No+Cover!'
+    }" alt="image" class="book-cover" data-id="${isbnList.id}" />
+    <div class="book-info">
+        <p class="title">${
+          isbnList.title.length > 11
+            ? isbnList.title.slice(0, 11) + '...'
+            : isbnList.title
+        }</p>
+    </div>
+`
 
-    //let nyCardContainer = document.querySelector(".nyList-container");
-    nyCardContainer.appendChild(cardNode); //finally apend node to the "cardContainer" node
+  //let nyCardContainer = document.querySelector(".nyList-container");
+  nyCardContainer.appendChild(cardNode) //finally apend node to the "cardContainer" node
 }
 
-export default displayBestSellerList;
+export default displayBestSellerList
--- book-lookUp/src/utils/getIsbns.js
+++ book-lookUp/src/utils/getIsbns.js
@@ -1,59 +1,107 @@
-import { googleApiKey } from "../app.js"
-import displayBestSellerList from "./displayBestSellerList.js";
+import { googleApiKey } from '../app.js'
+import displayBestSellerList from './displayBestSellerList.js'
 
-function getIsbns(nyList){
-    if(localStorage.getItem("nyTimesList")){
-        let currentList = JSON.parse(localStorage.getItem("nyTimesList"));
-        currentList.forEach((book)=>{
-            displayBestSellerList(book);
-        });
-    } else {
-        let newData = nyList.results.books;
-    
-        //iterate through the array and add only the isbn number into a new array
-        newData.forEach(item => {
-            let itemIsbnNumber = item.isbns[0].isbn10;
-            let isbnUrl = `https://www.googleapis.com/books/v1/volumes?q=isbn:${itemIsbnNumber}`;
-        
-            fetch(isbnUrl) //fetch each isbn number from google books api
-            .then(fetchedData => fetchedData.json())
-            .then((parsedData)=> {
+function getIsbns(nyList) {
+  if (localStorage.getItem('nyTimesList')) {
+    let currentList = JSON.parse(localStorage.getItem('nyTimesList'))
+    currentList.forEach((book) => {
+      displayBestSellerList(book)
+    })
+  } else {
+    let newData = nyList.results.books
 
-                //destructure object
-                let newParsedData = parsedData.items[0];
+    //iterate through the array and add only the isbn number into a new array
+    newData.forEach((item) => {
+      let itemIsbnNumber = item.isbns[0].isbn10
+      let isbnUrl = `https://www.googleapis.com/books/v1/volumes?q=isbn:${itemIsbnNumber}`
 
-                let {
-                    id,
-                    volumeInfo: {categories},
-                    volumeInfo: {title},
-                    volumeInfo: {subtitle = ""} = {},
-                    volumeInfo: {authors},
-                    volumeInfo: {imageLinks: {thumbnail = "https://dummyimage.com/70x100/00f/fff.png&text=No+Cover!"} = {}},
-                    volumeInfo: {description},
-                    volumeInfo: {language},
-                    volumeInfo: {pageCount},
-                    volumeInfo: {publishedDate},
-                    saleInfo: {buyLink},
-                } = newParsedData;
+      fetch(isbnUrl) //fetch each isbn number from google books api
+        .then((fetchedData) => fetchedData.json())
+        .then((parsedData) => {
+          //destructure object
+          let newParsedData = parsedData.items[0]
 
-                const isbn10Number = newParsedData.volumeInfo.industryIdentifiers[1].identifier;
+          let {
+            id,
+            volumeInfo: { categories },
+            volumeInfo: { title },
+            volumeInfo: { subtitle = '' } = {},
+            volumeInfo: { authors },
+            volumeInfo: {
+              imageLinks: {
+                thumbnail = 'https://dummyimage.com/70x100/00f/fff.png&text=No+Cover!',
+              } = {},
+            },
+            volumeInfo: { description },
+            volumeInfo: { language },
+            volumeInfo: { pageCount },
+            volumeInfo: { publishedDate },
+            saleInfo: { buyLink },
+          } = newParsedData
 
-                let nyLocalStorageList;
+          const isbn10Number =
+            newParsedData.volumeInfo.industryIdentifiers[1].identifier
 
-                if(localStorage.getItem("nyTimesList")){
-                    nyLocalStorageList = JSON.parse(localStorage.getItem("nyTimesList"));
-                    nyLocalStorageList.push({id, title, subtitle, authors, thumbnail, description, categories, language, pageCount, publishedDate, isbn10Number,buyLink});
-                } else {
-                    nyLocalStorageList = [{id, title, subtitle, authors, thumbnail, description, categories, language, pageCount, publishedDate, isbn10Number, buyLink}];
-                }
-                console.log(nyLocalStorageList)
-                localStorage.setItem("nyTimesList", JSON.stringify(nyLocalStorageList));
-                
-                displayBestSellerList({id, title, subtitle, authors, thumbnail, description, categories, language, pageCount, publishedDate, isbn10Number, buyLink});
+          let nyLocalStorageList
+
+          if (localStorage.getItem('nyTimesList')) {
+            nyLocalStorageList = JSON.parse(localStorage.getItem('nyTimesList'))
+            nyLocalStorageList.push({
+              id,
+              title,
+              subtitle,
+              authors,
+              thumbnail,
+              description,
+              categories,
+              language,
+              pageCount,
+              publishedDate,
+              isbn10Number,
+              buyLink,
             })
-            .catch((err) => console.log(err));
-        });
-    }
+          } else {
+            nyLocalStorageList = [
+              {
+                id,
+                title,
+                subtitle,
+                authors,
+                thumbnail,
+                description,
+                categories,
+                language,
+                pageCount,
+                publishedDate,
+                isbn10Number,
+                buyLink,
+              },
+            ]
+          }
+
+          localStorage.setItem(
+            'nyTimesList',
+            JSON.stringify(nyLocalStorageList)
+          )
+
+          displayBestSellerList({
+            id,
+            title,
+            subtitle,
+            authors,
+            thumbnail,
+            description,
+            categories,
+            language,
+            pageCount,
+            publishedDate,
+            isbn10Number,
+            buyLink,
+          })
+        })
+        .catch((err) => console.log(err))
+    })
+  }
 }
 
-export default getIsbns;
+export default getIsbns
